95118d9fd132bb6e234a6488e9ff3a5c11f80cae
Zmaroo <marlermi@hotmail.com>
2024-02-04 23:42:40 -0700

    r

--- zodch/charts/testSel.py
+++ zodch/charts/testSel.py
@@ -1,74 +1,24 @@
-# charts/tests.py
-from selenium.webdriver.common.by import By
-from selenium.webdriver.edge.service import Service
-from django.test import LiveServerTestCase
+
 from selenium import webdriver
-from selenium.webdriver.edge.options import Options
-import logging
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+import time
 
-# Configure logging
-logger = logging.getLogger(__name__)
-logging.basicConfig(filename='debug.log', level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
-logger.setLevel(logging.DEBUG)
+# Setup the Chrome WebDriver
+browser = webdriver.Chrome()
 
-class NatalChartSeleniumTestCase(LiveServerTestCase):
+# Open the natal.html page
+browser.get('http://localhost:8000/natal.html')
 
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        #service = Service('path/to/your/edgedriver')
-        options = Options()
-        # options.add_argument('headless')  # Uncomment for headless testing
-        cls.selenium = webdriver.Edge(options=options)
-        cls.selenium.implicitly_wait(10)
+# Fill in the birth date, time, and place
+browser.find_element_by_id('birth_date').send_keys('2000-01-01')
+browser.find_element_by_id('birth_time').send_keys('12:00')
+browser.find_element_by_id('birth_place').send_keys('New York, NY')
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.selenium.quit()  # Comment this line if you want to manually close the browser
-        super().tearDownClass()
+# Submit the form to render the natal chart
+browser.find_element_by_id('submit_button').click()
 
-    def test_display_natal_chart(self):
-        # Open the desired page
-        self.selenium.get(f'{self.live_server_url}')
-        
-        # Interactions with the page (e.g., filling out forms) go here
-         # Find the input fields and fill them
-        name_input = self.selenium.find_element(By.NAME, 'name')
-        birth_date_input = self.selenium.find_element(By.NAME, 'birth_date')
-        birth_time_input = self.selenium.find_element(By.NAME, 'birth_time')
-        location_input = self.selenium.find_element(By.NAME, 'location')
+# Wait for the chart to be rendered
+time.sleep(5)
 
-        name_input.send_keys('Michael Marler')
-        birth_date_input.send_keys('07-03-1973')
-        birth_time_input.send_keys('19:30')
-        location_input.send_keys('Salt Lake City, UT')
-
-         # Verify the values were set correctly
-        #logger.debug("Name entered: " + name_input('value'))
-        #logger.debug("Birth date entered: " + birth_date_input.get_attribute('value'))
-        #logger.debug("Birth time entered: " + birth_time_input.get_attribute('value'))
-        #logger.debug("Location entered: " + location_input.get_attribute('value'))
-        # ...
-
-        # Uncomment the following lines if needed
-        #WebDriverWait(self.selenium, 20).until(
-        #EC.presence_of_element_located((By.CSS_SELECTOR, "natal-chart-container"))
-        #)
-
-        # Debugging: Keep the browser open
-        input("Press Enter to quit the test...")  # Pauses here for manual intervention
-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-# This is a placeholder for the actual code that would autofill the form fields
-# Assuming 'driver' is a Selenium WebDriver instance and 'form_data' is a dictionary with form field names and values
-
-for field_name, value in form_data.items():
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, field_name)))
-    driver.find_element_by_name(field_name).send_keys(value)
-
+# Close the browser
+browser.quit()
